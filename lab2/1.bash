7eb79b2770d177d407c349862d5c93af53da9efc
MILKA-TOP <svakun@gmail.com>
2021-11-11 16:39:22 +0300

    lab2mod

--- lab2/1.bash
+++ lab2/1.bash
@@ -1,5 +1,5 @@
 #!bin/bash
-a=$(ps -fu root | awk '{print $2 ":" $8 "\n" }')
+a=$(ps -fu root | awk '{print $2 ":" $8 "\\n" }' | tail -n +2)
 #a=$(ps -fu root | awk '{print $2 ":" $11 "\n"}')
-echo $a | wc -w > pid1
+echo -e $a | wc -w > pid1
 echo  -e $a  >>pid1
--- lab2/4.bash
+++ lab2/4.bash
@@ -3,20 +3,19 @@ list=$(ps aux | awk '{print $2}' | sed 1d)
 outtext=""
 for pid in $list
     do
-	if [[ -f /proc/$pid/status && -f /proc/$pid/sched ]]
+        cp /proc/$pid/status status$pid 2>/dev/null
+        cp /proc/$pid/sched sched$pid 2>/dev/null
+	if [[ -s sched$pid ]] && [[ -s status$pid ]]
 	then
-	    cp /proc/$pid/status status$pid
-	    cp /proc/$pid/sched sched$pid
-
 	    ppid=$(grep "PPid" status$pid | awk '{print $2}')
 	    runtime=$(grep "sum_exec_runtime" sched$pid | awk '{print $3}')
 	    switches=$(grep "nr_switches" sched$pid | awk '{print $3}')
 	    art=$(echo $runtime " "  $switches | awk '{print $1 / $2}')
 	    outtext=$outtext$pid"\t"$ppid"\t"$art"\n"
-
+	fi
 	    rm -f status$pid
 	    rm -f sched$pid
-	fi
+
 done
 
 echo -e $outtext | sort -n -k2 | sed 1d | awk '{print "PID="$1"\t\tPPID="$2"\t\tART="$3}' > pid4
