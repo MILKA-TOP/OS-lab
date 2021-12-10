4ff1c09d430601397b9800eaa39ff9d70e698e2b
MILKA-TOP <svakun@gmail.com>
2021-12-10 13:55:48 +0300

    was modded 3lab_mod

--- lab3/mod/prog.c
+++ lab3/mod/prog.c
@@ -4,35 +4,62 @@
 #include <sys/mman.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <sys/types.h>
+
 
 int size_array=8;
-long int time_prog = 5;
+long int time_prog = 10;
+volatile sig_atomic_t usr_check = 0;
+
+void synch_signal (int sig) {
+     usr_check = 1;
+}
+
+
+void child_function(long* mem) {
+
+    long *now_array=mem;
+    int rand_num = rand();
+    now_array[rand_num % size_array]++;
+    memcpy(mem, now_array, sizeof(now_array));
+    kill(getppid(), SIGUSR1);
+
+    exit(0);
+}
 
 int main()
 {
     long *mem = mmap(NULL,size_array*sizeof(long), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
-    int pid = fork();
     long int start_time = time(NULL);
+
+    struct sigaction usr_action;
+    sigset_t block_mask;
+    sigfillset(&block_mask);
+    usr_action.sa_handler = synch_signal;
+    usr_action.sa_flags = 0;
+    sigaction(SIGUSR1, &usr_action, NULL);
+
     while (1) {
 	long *now_array = mem;
 	int rand_num = rand();
-
+	int pid = fork();
 	if (pid == 0)
-	    now_array[rand_num % size_array]++;
-	else
-	    now_array[(rand_num / size_array) % size_array]--;
+	    child_function(mem);
+	while (!usr_check) ;
 
+	now_array[(rand_num/size_array)%size_array]--;
 	memcpy(mem, now_array, sizeof(now_array));
+	usr_check=0;
 
 	if ((time(NULL) - start_time) >= time_prog) break;
-	sleep(0.2);
     }
-
-    if (pid == 0) exit(0);
 
     for (int i = 0; i < size_array; i++) {
 	printf("[%d] ", mem[i]);
     }
+
     printf("\n");
     return 0;
 }
